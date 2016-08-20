7c600ad18f5279b0467ed9aaabaf1fea56f7d840
Johnson Cheung <johnsoncheunghk06@yahoo.com.hk>
2016-08-20 23:26:53 +0800

    commit

--- fmtDt.js
+++ fmtDt.js
@@ -1,46 +1,46 @@
 exports = {fmtDt};
 'use strict';
-const sep_lin = '\r\n'
-const sep_col = ' | '
-const wdt_ay = (dt) => [0];
-const z_pad_ay = (ay,wdt_ay) => {
-    return ['']} // sldkf sldf
-const lin_h1 = (fld_ay,wdt_ay) => h1_fldAy_padded(fld_ay,wdt_ay).join(sep_lin)
-const lin_h2 = (fld_ay,wdt_ay) => h2_fldAy_padded(fld_ay,wdt_ay).join(sep_lin)
-const lin_bdy = (dr_ay,wdt_ay) => bdyLinAy_padded(dr_ay,wdt_ay).join(sep_lin)
-const bdyLinAy_padded = () => ['']
-const h2_fldAy = () => []
-const a = (1,2,3);
-const z_pad_val = (v,w,align) => {let a=1,b=2; return''}
-const c = z_pad_val(1,2,3)
-const dt_fldAy = () => $dt.fldAy
-const bdy_ay = () => {}
-const h1_ay =() => ['']
-const h2_ay = () => [""]
-const max = (...p) => {let n = p.length; if(n===0||n===1) return; var o=p[0]; for(var i=1;i<n;i++) o = o>p[i]?o:p[i]; return o }
-const hdr_val = (i) => typeof fld_ay(i) ==='string' ? fld_ay(i).length : ''
-const wdt_hdr = (i) => hdr_val(i).length
-const wdt_col = (i) => max( wdt_hdr(i), wdt_dtaCol(i))
+let $zip2 = (ay1,ay2) => []
+let a = (1,2,3);
+let bdyLinAy_padded = () => ['']
+let h1_ay =() => ['']
+let h2_ay = () => [""]
+let h2_fldAy = () => []
+let hdr_val = (i) => typeof fld_ay(i) ==='string' ? fld_ay(i).length : ''
+let lin_bdy = (dr_ay,wdt_ay) => bdyLinAy_padded(dr_ay,wdt_ay).join(sep_lin())
+let lin_h1 = (fld_ay,wdt_ay) => h1_fldAy_padded(fld_ay,wdt_ay).join(sep_lin())
+let lin_h2 = (wdt_ay) => {let f = lin_h2_fld_ay(wdt_ay); return z_pad_ay(f).join(sep_lin()) }
+let lin_h2_fld_ay = (wdt_ay) => ['']
+let max = (...p) => {let n=p.length; if(n===0||n===1)return; var o=p[0]; for(var i=1;i<n;i++) o = o>p[i]?o:p[i]; return o }
+let sep_col =() =>' | '
+let sep_lin =() => '\r\n'
+let wdt_ay = (dt) => [0];
+let wdt_col = (i) => max( wdt_hdr(i), wdt_dtaCol(i))
+let wdt_hdr = (i) => hdr_val(i).length
+let z_pad_ay = (ay,wdt_ay) => $zip2(ay,wdt_ay).reduce(z_pad_ay_reduce,[])   
+let z_pad_ay_reduce = (prvVal, curVal) => { const v=curVal[0],w=curVal[1], padded = pad(v,w,align); prvVal.push(padded); return prvVal }
+let z_pad_val = (v,w,align) => {let a=1,b=2; return''}
 // return array of string
 function fmtDt(dt) {
-    const wdt = wdt_ay(dt)
-    const fld = dt.fld_ay;
-    const quote = (s) => '| ' + s + ' |'
-    let h1 = lin_h1(fld,wdt); h1 = quote(h1)
-    let h2 = lin_h2(fld,wdt); h2 = quote(h2)
-    const x = lin_bdy(fld,wdt); 
-    const o = [];
-    o.push(h1, h2, h1, x, h2);
+    let wdt = wdt_ay(dt)
+    let fld = dt.fldNm_ay
+    let quote = (s) => '| ' + s + ' |'
+    let h1 = lin_h1(fld,wdt)
+    let h2 = lin_h2(wdt)
+    let o1 = quote(h2)
+    let o2 = quote(h1)
+    let ox = lin_bdy(fld,wdt); 
+    let o = [];
+    o.push(o1, o2, o1, ox, o1);
     return o
 }
 function test() {
-    let fld_ay = ['col-a','col-b', 'col-c']
-    fld_ay = ['col-a']
+    let fldNm_ay = ['col-a','col-b', 'col-c']
     let dr1 = [1,2,3]
     let dr2 = [3,4,5]
     let dr3 = ["sdf","skldf","lksd"]
     let dr_ay = [dr1,dr2,dr3]
-    let a = fmtDt({fld_ay, dr_ay})
+    let a = fmtDt({fldNm_ay, dr_ay})
     debugger;
 }
 test();
