b866359c085b98a01a37a77b3451ae8e12416311
Johnson Cheung <johnsoncheunghk06@yahoo.com.hk>
2016-08-23 00:48:18 +0800

    ok

--- fmtDt.js
+++ fmtDt.js
@@ -29,6 +29,7 @@ const pad_left=(s,l,w) => s+pad_spc(l,w)
 const pad_right=(s,l,w) => pad_spc(l,w)+s
 const pad_centre=(s,l,w) =>{const [h1,h2]=[pad_half_spc1(l,w)]}
 const pad = (s, w, align) => {
+    s=String(s)
     const l = s.length
     const ovr_flw = l>w
     if(ovr_flw)return pad_ovr_flw(s,l,w)
@@ -38,8 +39,8 @@ const pad = (s, w, align) => {
         default: return pad_left(s,l,w)
     }
 }
-console.log(pad('sdfdfsdklfj lskdf lksdj flsjdf',50,2))
-debugger
+console.log(pad('sdfdfsdj flsjdf',50)+'<')
+
 const zip = (...ay) => {
     const ay1 = ay.map(ay=>Array.isArray(ay)?ay:[]) // 
     const nEle = ay1.reduce((rslt,ay)=>max(rslt,ay.length),0)
@@ -52,38 +53,42 @@ const zip = (...ay) => {
 const ay_maxLen=(strAy) =>  strAy.reduce((rslt,itm)=> max(rslt,itm.length),0)
 const pipe = (i,...f) => f.reduce((c,p)=>c(p),i)
 const branch = (i, ...f) => f.reduce((c, p) => p.push(c(i), []))
-//=================================================================
 const SEP_COL = ' | '
 const SEP_LIN = '\r\n'
-const bdy_$lin = (dr, wdt_ay, align_ay) => {const fld = bdy_$lin_fld(dr, wdt_ay, align_ay); return lin_$join(fld)}
-const bdy_$lin_fld = (dr, wdt_ay, align_ay) =>dr.map((v,i)=>pad(v,wdt_ay[i],align_ay[i]))
-const bdy_linAy = (dta, wdt_ay, align_ay) => dta.map(dr=>bdy_$lin(dr, wdt_ay, align_ay))
-const h1_fld = (wdt_ay) => wdt_ay.map(wdt=>'-'.repeat(wdt))
-const h2_fld = (fld_ay, wdt_ay, align_ay) => fld_ay.map((fldNm,i)=>pad(fldNm, wdt_ay[i], align_ay[i]))
-const lin_$join = (fld) => '| ' + fld.join(SEP_COL) + ' |'
-const lin_bdy = (bdy_linAy) => bdy_linAy.join(SEP_LIN)
-const lin_h1 = (h1_fld) => lin_$join(h1_fld)
-const lin_h2 = (h2_fld) => lin_$join(h2_fld)
-const wdt_ay = (wdt_ay_h2, wdt_ay_col) =>{const h = wdt_ay_h2; return h.map((h,idx)=>max(h,wdt_ay_col[idx]))}
-const wdt_ay_col = (fld_ay, dta) => fld_ay.map((fld,idx)=>wdt_$colI(dta,idx))
-const wdt_ay_h2 = (fld_ay) => fld_ay.map(fld=>fld.length)
-const wdt_$colI_col = (dta,idx) => dta.map(dr=>dr[idx])
-const wdt_$colI = (dta,idx) => ay_maxLen(wdt_$colI_col(dta,idx))
-const z =(lin_h1,lin_h2,lin_bdy) => {const h1=lin_h1; return [h1, lin_h2, h1, lin_bdy, h1].join(SEP_LIN)}
-
-const fmtDt = (dt, align_ay) => {
-    const dta$ = dt.dta
-    const fld_ay$ = dt.fldNm_ay
-    const align_ay$ = align_ay || []
-    const wdt_ay_col$ = wdt_ay_col(fld_ay$, dta$)
-    const wdt_ay_h2$ = wdt_ay_h2(fld_ay$)
-    const wdt_ay$ = wdt_ay(wdt_ay_h2$, wdt_ay_col$)
-    const h1_fld$ = h1_fld(wdt_ay$)
-    const h2_fld$ = h2_fld(fld_ay$, wdt_ay$, align_ay$)
-    const bdy_linAy$ = bdy_linAy(dta$, wdt_ay$, align_ay$)
-    const lin_h1$ = lin_h1(h1_fld$)
-    const lin_h2$ = lin_h2(h2_fld$)
-    const lin_bdy$ = lin_bdy(bdy_linAy$)
-    return oup(lin_h1$, lin_h2$, lin_bdy$)
-}
+//compute-beg ==========================================================
+let fmtDt; {
+    const _Inp = (dt, align_ay=[]) => {}
+    const _Oup = (lin_h1,lin_h2,lin_bdy) => {const h1=lin_h1; return [h1, lin_h2, h1, lin_bdy, h1].join(SEP_LIN)}
+    const bdy_$lin = (dr, wdt_ay, align_ay) => {const fld = bdy_$lin_fld(dr, wdt_ay, align_ay); return lin_$join(fld)}
+    const bdy_$lin_fld = (dr, wdt_ay, align_ay) => wdt_ay.map((w,i)=>pad(dr[i],w,align_ay[i]))
+    const bdy_linAy = (dta, wdt_ay, align_ay) => dta.map(dr=>bdy_$lin(dr, wdt_ay, align_ay))
+    const h1_fld = (wdt_ay) => wdt_ay.map(wdt=>'-'.repeat(wdt))
+    const h2_fld = (fld_ay, wdt_ay, align_ay) => fld_ay.map((fldNm,i)=>pad(fldNm, wdt_ay[i], align_ay[i]))
+    const lin_$join = (fld) => '| ' + fld.join(SEP_COL) + ' |'
+    const lin_bdy = (bdy_linAy) => bdy_linAy.join(SEP_LIN)
+    const lin_h1 = (h1_fld) => lin_$join(h1_fld)
+    const lin_h2 = (h2_fld) => lin_$join(h2_fld)
+    const wdt_ay = (wdt_ay_h2, wdt_ay_col) =>{const h = wdt_ay_h2; return h.map((h,idx)=>max(h,wdt_ay_col[idx]))}
+    const wdt_ay_col = (fld_ay, dta) => fld_ay.map((fld,idx)=>wdt_$colI(dta,idx))
+    const wdt_ay_h2 = (fld_ay) => fld_ay.map(fld=>fld.length)
+    const wdt_$colI_col = (dta,idx) => dta.map(dr=>String(dr[idx]))
+    const wdt_$colI = (dta,idx) => ay_maxLen(wdt_$colI_col(dta,idx))
+    //compute-mid
+    const compute = (dt, align_ay=[]) => {
+        const dta$ = dt.dta
+        const fld_ay$ = dt.fldNm_ay
+        const align_ay$ = align_ay
+        const wdt_ay_col$ = wdt_ay_col(fld_ay$, dta$)
+        const wdt_ay_h2$ = wdt_ay_h2(fld_ay$)
+        const wdt_ay$ = wdt_ay(wdt_ay_h2$, wdt_ay_col$)
+        const h1_fld$ = h1_fld(wdt_ay$)
+        const h2_fld$ = h2_fld(fld_ay$, wdt_ay$, align_ay$)
+        const bdy_linAy$ = bdy_linAy(dta$, wdt_ay$, align_ay$)
+        const lin_h1$ = lin_h1(h1_fld$)
+        const lin_h2$ = lin_h2(h2_fld$)
+        const lin_bdy$ = lin_bdy(bdy_linAy$)
+        return _Oup(lin_h1$, lin_h2$, lin_bdy$)
+    }
+    fmtDt = compute }
+//compute-end
 exports.fmtDt = fmtDt
--- test/test-fmtDt.js
+++ test/test-fmtDt.js
@@ -1,8 +1,9 @@
 const {fmtDt} = require('../fmtDt.js')
-let fldNm_ay = ['col-a','col-b', 'col-c']
+let fldNm_ay = ['col-a','col-b', 'col-c','a','b','c']
 let dr1 = [1,2,3]
-let dr2 = [3,4,5]
-let dr3 = ["sdf","skldf","lksd"]
+let dr2 = [3,'lksdfj lksdjf sdf',5]
+let dr3 = ["sdf","skldf","lksdd dfdf"]
 let dta = [dr1,dr2,dr3]
 let a = fmtDt({fldNm_ay, dta})
+console.log(a)
 debugger
