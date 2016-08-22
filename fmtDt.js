b4ed848d83fa1f0d0c8ce648a9f7fd449f9c3011
Johnson Cheung <johnsoncheunghk06@yahoo.com.hk>
2016-08-22 23:49:50 +0800

    quite good

--- fmtDt.js
+++ fmtDt.js
@@ -9,12 +9,25 @@ const max = (...p) => {
 }
 
 //===========
-const pad_ovr_flw=(s,l,w) => {}
-const pad_spc=(l,w) => {}
-const pad_half_spc1=(l,w) => {}
-const pad_left=(s,l,w) => {}
-const pad_right=(s,l,w) => {}
-const pad_centre=(s,l,w) =>{}
+const LEFT = 1
+const CENTRE = 2
+const RIGHT = 3
+const pad_ovr_flw=(s,l,w) => {
+    switch(w) {
+        case 1: return '.'
+        case 2: return '..'
+        case 3: return s.charAt(0)+'..'
+        default: return s.substr(0,w-2)+'..'
+    }
+}
+const pad_spc=(l,w) =>' '.repeat(w-l)
+const pad_half_len1=(l,w) => ((w-l)/2).toFixed()
+const pad_half_len2=(l,w) => w-pad_half_len1(l,w)
+const pad_half_spc1=(l,w) => ' '.repeat(pad_half_len1(l,w))
+const pad_half_spc2=(l,w) => ' '.repeat(pad_half_len2(l,w))
+const pad_left=(s,l,w) => s+pad_spc(l,w)
+const pad_right=(s,l,w) => pad_spc(l,w)+s
+const pad_centre=(s,l,w) =>{const [h1,h2]=[pad_half_spc1(l,w)]}
 const pad = (s, w, align) => {
     const l = s.length
     const ovr_flw = l>w
@@ -25,6 +38,8 @@ const pad = (s, w, align) => {
         default: return pad_left(s,l,w)
     }
 }
+console.log(pad('sdfdfsdklfj lskdf lksdj flsjdf',50,2))
+debugger
 const zip = (...ay) => {
     const ay1 = ay.map(ay=>Array.isArray(ay)?ay:[]) // 
     const nEle = ay1.reduce((rslt,ay)=>max(rslt,ay.length),0)
@@ -40,23 +55,21 @@ const branch = (i, ...f) => f.reduce((c, p) => p.push(c(i), []))
 //=================================================================
 const SEP_COL = ' | '
 const SEP_LIN = '\r\n'
-const bdy_$lin = (zip) => {const [val_ay, wdt_ay, align_ay] = zip; return bdy_$lin_bld(val_ay, wdt_ay, align_ay)}
-const bdy_$lin_bld = (val_ay, wdt_ay, align_ay) => {const {v,w,a}=arguments; v.map((v,i)=>pad(v,w(i),a(i)))}
-const bdy_linAy = (bdy_zipAy) => bdy_zipAy.map(bdy_$lin)
-const bdy_zipAy = (dta, wdt_ay, align_ay) => dta.map(dr=>zip(dr,wdt_ay,align_ay))
+const bdy_$lin = (dr, wdt_ay, align_ay) => {const fld = bdy_$lin_fld(dr, wdt_ay, align_ay); return lin_$join(fld)}
+const bdy_$lin_fld = (dr, wdt_ay, align_ay) =>dr.map((v,i)=>pad(v,wdt_ay[i],align_ay[i]))
+const bdy_linAy = (dta, wdt_ay, align_ay) => dta.map(dr=>bdy_$lin(dr, wdt_ay, align_ay))
 const h1_fld = (wdt_ay) => wdt_ay.map(wdt=>'-'.repeat(wdt))
-const h2_fld = (h2_zip) => h2_zip.map((padArg)=>pad.apply(null,padArg))
-const h2_zip = (fld_ay,wdt_ay,align_ay) => {const [f,w,a]=arguments; return zip(f,w,a)}
-const lin_$join = (fld) => fld.join(SEP_COL)
+const h2_fld = (fld_ay, wdt_ay, align_ay) => fld_ay.map((fldNm,i)=>pad(fldNm, wdt_ay[i], align_ay[i]))
+const lin_$join = (fld) => '| ' + fld.join(SEP_COL) + ' |'
 const lin_bdy = (bdy_linAy) => bdy_linAy.join(SEP_LIN)
-const lin_h1 = (h1_fld) => $join(h1_fld)
-const lin_h2 = (h2_fld) => $join(h2_fld)
-const wdt_ay = (wdt_ay_h2, wdt_ay_col) => {const [h,c]=arguments; return h.map((h,idx)=>max(h,c(idx)))}
+const lin_h1 = (h1_fld) => lin_$join(h1_fld)
+const lin_h2 = (h2_fld) => lin_$join(h2_fld)
+const wdt_ay = (wdt_ay_h2, wdt_ay_col) =>{const h = wdt_ay_h2; return h.map((h,idx)=>max(h,wdt_ay_col[idx]))}
 const wdt_ay_col = (fld_ay, dta) => fld_ay.map((fld,idx)=>wdt_$colI(dta,idx))
 const wdt_ay_h2 = (fld_ay) => fld_ay.map(fld=>fld.length)
 const wdt_$colI_col = (dta,idx) => dta.map(dr=>dr[idx])
 const wdt_$colI = (dta,idx) => ay_maxLen(wdt_$colI_col(dta,idx))
-const z = (lin_h1,lin_h2,lin_bdy) => {const [h1,h2,bdy]=arguments; return [h1,h2,h1,bdy,h1].join(SEP_LIN)}
+const z =(lin_h1,lin_h2,lin_bdy) => {const h1=lin_h1; return [h1, lin_h2, h1, lin_bdy, h1].join(SEP_LIN)}
 
 const fmtDt = (dt, align_ay) => {
     const dta$ = dt.dta
@@ -64,12 +77,10 @@ const fmtDt = (dt, align_ay) => {
     const align_ay$ = align_ay || []
     const wdt_ay_col$ = wdt_ay_col(fld_ay$, dta$)
     const wdt_ay_h2$ = wdt_ay_h2(fld_ay$)
-    const wdt_ay$ = wdt_ay(fld_ay$, dta$)
-    const h1_fld$ = h1_fld(wdt_ay)
-    const h2_zip$ = h2_zip(fld_ay$, wdt_ay$, align_ay$)
-    const h2_fld$ = h2_fld(h2_zip$)
-    const bdy_zipAy$ = bdy_zipAy(dta$, wdt_ay$, align_ay$)
-    const bdy_linAy$ = bdy_linAy(bdy_zipAy$)
+    const wdt_ay$ = wdt_ay(wdt_ay_h2$, wdt_ay_col$)
+    const h1_fld$ = h1_fld(wdt_ay$)
+    const h2_fld$ = h2_fld(fld_ay$, wdt_ay$, align_ay$)
+    const bdy_linAy$ = bdy_linAy(dta$, wdt_ay$, align_ay$)
     const lin_h1$ = lin_h1(h1_fld$)
     const lin_h2$ = lin_h2(h2_fld$)
     const lin_bdy$ = lin_bdy(bdy_linAy$)
