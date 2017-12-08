5648bab9d1dfba002fd7754e17e94fd402949d50
JohnsonCheung <johnsoncheunghk06@yahoo.com.hk>
2017-12-08 16:57:07 +0800

    Update lib.js

--- lib.js
+++ lib.js
@@ -21,3 +21,28 @@ function doc_ids(doc) {
   }
   return ids
 }
+function el_add_atr(el,nm,v) {
+  var a = el.ownerDocument.createAttribute(nm)
+  a.value = v
+  el.setNamedItem(a)
+}
+function doc_add_id(doc) {
+  doc = doc||document
+  var all = doc.all
+  var tagName_cnt_dic = {}
+  for(var j=0;j<all.length;j++) {
+    var el = all[j]
+    var id = el.id
+    if(!id) {
+      var tagName = el.tagName
+      var idVal = _idVal(tagName, tagName_cnt_dic)
+      el_add_atr(el,"id",idVal)
+    }
+  }
+  function _idVal(tagName, tagName_cnt_dic) {
+    if(tagName_cnt_dic[tagName]===undefined) 
+      tagName_cnt_dic[tagName]=0
+    return ++tagName_cnt_dic[tagName]
+  }
+}
+    
