eb32b979163bb8136753a7f10a13c6b19231fb6b
JohnsonCheung <johnsoncheunghk06@yahoo.com.hk>
2017-12-08 16:18:07 +0800

    Update lib.js

--- lib.js
+++ lib.js
@@ -1,1 +1,10 @@
-aa
+function doc_tagNames(doc) {
+  var all = doc.all
+  var ny = {}
+  for(var j=0;j<all.length;j++) { 
+    var nm = all[j].tagName
+    if(!ny[nm])
+      ny[nm]=true
+  }
+  return Object.getOwnPropertyNames(ny)
+}
