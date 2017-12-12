a3408f29fe53fe95589b560290b65e7935b6b14e
JohnsonCheung <johnsoncheunghk06@yahoo.com.hk>
2017-12-13 04:34:38 +0800

    Update enm.js

--- enm.js
+++ enm.js
@@ -1,4 +1,4 @@
-const max = enm => {
+const enmMax = enm => {
   let o;
   for(let i of enum) {
     if(o===undefined) {
@@ -10,7 +10,7 @@ const max = enm => {
     }
   }
 }
-const min = enm => {
+const enmMin = enm => {
   let o;
   for(let i of enum) {
     if(o===undefined) {
@@ -22,10 +22,43 @@ const min = enm => {
     }
   }
 }
-const map1 = f => enm => {
+const enmMap = f => enm => {
   let o =[]
   for(let i of enm) {
     o.push(f(i))
   }
   return o
+}
+const compose = (...f) => p => {
+  for(i of f) p = i(p)
+  return p
+}
+const pipe = (p,...f) = compose(...f)(p)
+const rmvFstChr = s => s.substr(1)
+const rmvLasChr = s => s.substr(0,s.length-1)
+const left = n => s => s.substr(0,n)
+const len = s => s.length
+const right = n => s => s.substr(len(s)-n,n)
+const mid = ix => s => s.substr(ix)
+const midPos = ix => len => s => s.substr(ix,len)
+const eq => a => b => a === b
+const ne => a => b => a !== b
+const allTrue = ay => {
+  for(i of ay) {
+    if(!i) return false
+  }
+  return true
+}
+const allFalse = ay => {
+  for(i of ay) {
+    if(i) return false
+  }
+  return true
+}
+const prp => pth => o => {
+  try {
+    return eval("o" + pth)
+  } catch(e) {
+    return undefined
+  }
 }
