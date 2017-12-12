a6f42beb57a79bcba25e9560241c0049ad91d52b
JohnsonCheung <johnsoncheunghk06@yahoo.com.hk>
2017-12-13 02:58:44 +0800

    Update fmtDt.js

--- fmtDt.js
+++ fmtDt.js
@@ -3,11 +3,27 @@ const $join_fld = fld => '| ' + fld.join(SEP_COL) + ' |'
 const $col_w = (dta, i) => maxLen$(_col_i(dta, i))
 const _col_i = (dta, i) => dta.map(dr => String(dr[i]))
 const $bdy_lin = (dr, wdt, align) => $join_fld(wdt.map((w, i) => pad$(dr[i], w, align[i])))
-const {pad, ay_maxLen, max, SEP_COL, SEP_LIN} = require('util1')
-const [maxLen$,max$,pad$] = [ay_maxLen,max,pad]
+const {pad, ay_maxLen, SEP_COL, SEP_LIN} = require('util1')
+const pad = s => s
+const ayMax = ay => { 
+    let o
+    for(i of ay) {
+        if(o===undefined) {
+            o = i
+        } else {
+            if(i>o) {
+                o = i
+            }
+        }
+    }
+    return o
+}
+const len = i => i.length
+const ay_maxLen = ay => ayMax(ay.map(len))
+const max = (a,b) => a>b ? a : b
 const fmtDt = (dt, align = []) => {
     const {fld,dta) = dt
-    const wdt = fld.map((_, i) => max$($col_w(dta, i), fld[i].length))
+    const wdt = fld.map((_, i) => max($col_w(dta, i), fld[i].length))
     const bdy_linAy = dta.map(dr => $bdy_lin(dr, wdt, align))
     const bdy = bdy_linAy.join(SEP_LIN)
     const h2fld = fld.map((fld, i) => pad$(fld, wdt[i], align[i]))
