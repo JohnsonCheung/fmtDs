8c60a87a1c7a98b4b0106d31434c49e879e955aa
JohnsonCheung <johnsoncheunghk06@yahoo.com.hk>
2017-12-13 10:41:58 +0800

    Update fmtDt.js

--- fmtDt.js
+++ fmtDt.js
@@ -2,8 +2,10 @@
 const $join_fld = fld => '| ' + fld.join(SEP_COL) + ' |'
 const $col_w = (dta, i) => maxLen$(_col_i(dta, i))
 const _col_i = (dta, i) => dta.map(dr => String(dr[i]))
-const $bdy_lin = (dr, wdt, align) => $join_fld(wdt.map((w, i) => pad$(dr[i], w, align[i])))
-const {pad, ay_maxLen, SEP_COL, SEP_LIN} = require('util1')
+const jn=sep=>ay=>ay.join(sep)
+const jnCrLf=jn('\r\n')
+const jnLf=jn('\n')
+const sdr2lin = (wdtAy, align, sep) => sdr => jn(sep)(wdtAy.map((w, i) => pad(sdr[i], w, align[i])))
 const pad = s => s
 const ayMax = ay => { 
     let o
@@ -22,9 +24,11 @@ const len = i => i.length
 const ay_maxLen = ay => ayMax(ay.map(len))
 const max = (a,b) => a>b ? a : b
 const fmtDt = (dt, align = []) => {
-    const {fld,dta) = dt
-    const wdt = fld.map((_, i) => max($col_w(dta, i), fld[i].length))
-    const bdy_linAy = dta.map(dr => $bdy_lin(dr, wdt, align))
+    const {fny,dry} = dt
+    Const sdry = dryToSdry(dry)
+    const sdryColWdtAy = sdry2ColWdtAy(sdry)
+    const wdtAy = fny.map((f, i) => max(sdryColWdtAy[i], f.length))
+    const bdyLinAy = sdry.map(sdrToLin(wdtAy, align))
     const bdy = bdy_linAy.join(SEP_LIN)
     const h2fld = fld.map((fld, i) => pad$(fld, wdt[i], align[i]))
     const h2 = $join_fld(h2fld)
